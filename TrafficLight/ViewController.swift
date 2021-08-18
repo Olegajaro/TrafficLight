f9b37d2c1deb3942d85df2713e22a356a1d80b1a
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-08-18 17:57:58 +0300

    Unfinished Main with logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var switchColorViewButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 60
+        yellowView.layer.cornerRadius = 60
+        greenView.layer.cornerRadius = 60
+        redView.alpha = 0.2
+        yellowView.alpha = 0.2
+        greenView.alpha = 0.2
+        switchColorViewButton.layer.cornerRadius = 11
     }
 
-
+    @IBAction func switchColorButtonPressed() {
+        switchColorViewButton.setTitle("Next", for: .normal)
+        
+        if redView.alpha == yellowView.alpha {
+            redView.alpha = CGFloat(MAXFLOAT)
+            greenView.alpha = 0.2
+        }
+        else if redView.alpha > 0.2 && yellowView.alpha == greenView.alpha {
+            yellowView.alpha = CGFloat(MAXFLOAT)
+            redView.alpha = 0.2
+        }
+        else if yellowView.alpha > 0.2 && greenView.alpha == redView.alpha {
+            greenView.alpha = CGFloat(MAXFLOAT)
+            yellowView.alpha = 0.2
+        }
+    }
+    
 }
 
